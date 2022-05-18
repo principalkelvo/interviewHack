2f4e2e9e974ab451c8d2f1f2afb02d332634229b
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-18 12:39:29 +0300

    validates password and checks user in db

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,4 +1,9 @@
-
+const people = [
+    {
+        userName: "admin@interviewhack.com",
+        password: "adminHack",
+    },
+];
 
 
 var login = document.getElementById("login");
@@ -17,6 +22,9 @@ function validateEmail(emailValidate) {
 login.addEventListener('click', function() {
     var emailValidate=emailField.value;
     passwordField= passwordField.value;
+
+    // console.log(passwordField.length)
+
     if(!validateEmail(emailValidate)){
         emailField.style.borderColor = "red";
         alert("Email is not valid");
@@ -28,11 +36,36 @@ login.addEventListener('click', function() {
     if(passwordField.length<6){
         passwordField.style.borderColor = "red";
         alert("Password is too short");
+        return;
+    }else{
+        console.log("Password is valid");
+    }
+
+    for(var i=0;i<people.length;i++){
+        if(people[i].userName==emailValidate && people[i].password==passwordField){
+            alert("Login Successful");
+            window.location.href = "../../index.html";
+        }else{
+            emailField.style.borderColor = "red";
+            passwordField.style.borderColor = "red";    
+            alert("Login Failed");
+            // window.location.href = "../pages/register.html";
+
+        }
     }
     
     console.log('login');
-});
+})
 
 register.addEventListener('click', function() {
+    var emailValidate=emailField.value;
+    if(!validateEmail(emailValidate)){
+        emailField.style.borderColor = "red";
+        alert("Email is not valid");
+        return;
+    }
+    else{
+        console.log("Email is valid");
+    }
     console.log('register');
 });
