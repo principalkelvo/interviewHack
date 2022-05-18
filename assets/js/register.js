d99ae525147dc18a668f2db0fd1007989a8d39dd
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-05-18 11:27:16 +0300

    tests butons

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,2 +1,10 @@
 var login = document.getElementById('login');
 var register = document.getElementById('register');
+
+login.addEventListener('click', function() {
+    console.log('login');
+});
+
+register.addEventListener('click', function() {
+    console.log('register');
+});
